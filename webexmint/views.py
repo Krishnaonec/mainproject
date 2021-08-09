fc3c6d6f6166e7c990762efc986f10f130551abe
fayaz <fayazalem24@gmail.com>
2021-08-09 17:23:07 +0530

    Add Webex OAuth grant flow

--- webexmint/views.py
+++ webexmint/views.py
@@ -7,19 +7,25 @@ from decouple import config
 from .models import UserOwnerSpace
 
 def oauth(request):
-    # if 'code' in request.GET and request.GET.get('state') == config('state'):
-    #     code = request.GET.get('code')
-    #     api  = WebexTeamsAPI.from_oauth_code(
-    #         client_id     = config('client_id'),
-    #         client_secret = config('client_secret'),
-    #         code          = code,
-    #         redirect_uri  = config('redirect_uri')
-    #     )
+    if 'code' in request.GET and request.GET.get('state') == config('state'):
+        code = request.GET.get('code')
+        try:
+            api  = WebexTeamsAPI.from_oauth_code(
+                client_id     = config('client_id'),
+                client_secret = config('client_secret'),
+                code          = code,
+                redirect_uri  = config('redirect_uri')
+            )
 
-    # request.session['access_token'] = api.access_token
-    request.session['access_token'] = 'YTg3YzVkMDAtODA1NC00ZTQ4LTllMzItODM4ZTkzY2FlMzE5MmQ3NWNhMmItYjY0_P0A1_72fd5250-f4c5-4261-ba8d-0385199b4781'
-    messages.success(request, f"You webex authentication is successful! Now you can contact onwers.")
-    return redirect('home')
+            request.session['access_token'] = api.access_token
+            messages.success(request, f"You webex authentication is successful! Now you can contact onwers.")
+            return redirect('home')
+        except:
+            messages.error(request, f"Something went wrong! Please try again")
+            return redirect('home')
+    else:
+        messages.error(request, f"Something went wrong! Please try again")
+        return redirect('home')
 
 
 def contact_owner(request, owner_id):
